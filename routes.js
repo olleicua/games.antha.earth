08301b7fadb959495fbd4ede7938a13121b0e740
Glitch (3d-connect-4) <none>
2021-04-12 01:46:07 +0000

    🐜📒 Checkpoint ./routes.js:17596236/1052 ./app.js:17596236/307 ./README.md:17596236/3050 ./server.js:17596236/336

--- routes.js
+++ routes.js
@@ -1,18 +1,16 @@
-const app = require('./app.js');
-
 const routes = [
   ['/', '/views/index.html'],
   ['/chat', '/views/index.html'],
 ];
 
 module.exports = {
-  listen: 
+  registerListeners: (app) => {
+    for (var i = 0; i < routes.length; i++) {
+      var requestedPath, staticFilePath;
+      [requestedPath, staticFilePath] = routes[i];
+      app.get(requestedPath, (request, response) => {
+        response.sendFile(__dirname + staticFilePath);
+      });
+    }
+  }
 };
-
-for (var i = 0; i < routes.length; i++) {
-  var requestedPath, staticFilePath;
-  [requestedPath, staticFilePath] = routes[i];
-  app.get(requestedPath, (request, response) => {
-    response.sendFile(__dirname + staticFilePath);
-  });
-}
--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 const WebSocket = require('ws');
 var expressWs = require('express-ws')(app);
-
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
