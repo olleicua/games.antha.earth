532e11425960bfec1bce16977d9ea0f56fc90d3d
Glitch (3d-connect-4) <none>
2021-04-25 20:26:42 +0000

    🌲🗾 Checkpoint ./server.js:17596236/8 ./.glitch-assets:17596236/532 ./public/style.css:17596236/316

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-require('./staticRoutes.js').connect(app)
+require('./routes.js').connect(app)
 
 require('./chatWs.js')(app);
 
