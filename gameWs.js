268c46403f811b2dc9c47b50fc3799d4a452ec88
Glitch (3d-connect-4) <none>
2021-04-28 21:01:51 +0000

    🌄🎅 Checkpoint ./gameWs.js:17596236/926

---
+++ gameWs.js
@@ -0,0 +1,36 @@
+const WebSocket = require('ws');
+
+const clients = [];
+
+const gameState = [];
+for (let x = 0; x < 4; x ++) {
+  gamestate.push([]);
+  for (let y = 0; y < 4; y ++) {
+    gamestate[x].push([]);
+    for (let z = 0; z < 4; z ++) {
+      gamestate[x][y].push(0);
+    }
+  }
+}
+let redPlayer = null;
+let greenPlayer = null;
+
+module.exports = (app) => {
+  app.ws('/chat', (client, request) => {
+    //client.id = clients.length
+    clients.push(client);
+
+    client.send(JSON.stringify(messages));
+
+    client.on('message', (data) => {
+      const message = JSON.parse(data);
+      messages.push(message);
+
+      clients.forEach((target) => {
+        if (target.readyState === WebSocket.OPEN) {
+          target.send(JSON.stringify([message]));
+        }
+      });
+    });
+  });
+};
